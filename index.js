b381c8638bf68bd94c5b2cdd521a871c40b34a47
Faizan Dhumadia <87863621+faizan-dhumadia@users.noreply.github.com>
2022-07-03 22:15:18 +0530

    Cors 0

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 const connectToMongo = require('./db');
 const express = require('express')
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
 var cors = require('cors')
 const corsOption = {
-    originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
+    origin: '*',
+    credentials: true,
 }
 connectToMongo();
 const app = express()
