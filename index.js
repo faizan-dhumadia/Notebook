7cc273394f21f614ade2c33d51e97aed42700f2e
Faizan Dhumadia <87863621+faizan-dhumadia@users.noreply.github.com>
2022-07-03 22:33:40 +0530

    Cors 1

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const corsOption = {
 connectToMongo();
 const app = express()
 const port = process.env.PORT || 5000
-app.use(cors(corsOption))
+    // app.use(cors(corsOption))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.json())
 
 //Available Routes
