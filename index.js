252755a416b29464457736d24dcb7eb5457ed610
Faizan Dhumadia <87863621+faizan-dhumadia@users.noreply.github.com>
2022-07-03 23:01:28 +0530

    Cors 2

--- index.js
+++ index.js
@@ -3,18 +3,15 @@ const express = require('express')
 
 var cors = require('cors')
 const corsOption = {
-    origin: '*',
-    credentials: true,
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
 }
 connectToMongo();
 const app = express()
 const port = process.env.PORT || 5000
-    // app.use(cors(corsOption))
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors(corsOption))
 app.use(express.json())
 
 //Available Routes
